bfc38cc149d6f49b8cc76707034377376174afcc
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-07-26 15:34:03 -0400

    Added IO Services to contracts
    these are described a little more in Issues #18  and #17

---
+++ Contracts/ICANTalonIOService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    /// <summary>
+    /// The CANTalon has compound IO, therefore it needs its own type
+    /// </summary>
+    public interface ICANTalonIOService : IIOService
+    {
+        IIOService AnalogInput { get; }
+        IIOService DigitalInput { get; }
+        IIOService LimitSwitchFront { get; }
+        IIOService LimitSwitchRear { get; }
+    }
+}
---
+++ Contracts/IRobotIOService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    /// <summary>
+    /// Represents the state of the robot IO; separate from the robot model, but accessible to it
+    /// </summary>
+    public interface IRobotIOService
+    {
+        IDictionary<uint, IIOService> PWMOutputs { get; }
+        IDictionary<uint, IIOService> AnalogInputs { get; }
+        IDictionary<uint, IIOService> RelayOutputs { get; }
+        IDictionary<uint, IIOService> DigitalInputs { get; }
+        IDictionary<uint, ICANTalonIOService> CANTalons { get; }
+    }
+}
