e36563cd48e3dcd92d43377bdb19c329347f08dc
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-07-31 12:04:47 -0400

    Removed Model Dependency on Contracts

--- Models/ContinuousCommand.cs
+++ Models/ContinuousCommand.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Team1922.MVVM.Framework;
-using Team1922.MVVM.Models.DynamicParams;
 
 namespace Team1922.MVVM.Models
 {
