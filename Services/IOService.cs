14e0414ff5ca44ccc24d573be677cfc86e047d11
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-07-31 12:27:24 -0400

    Small addition to last commit

--- Services/IOService.cs
+++ Services/IOService.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Team1922.Contracts;
+using Team1922.MVVM.Contracts;
 
 namespace Team1922.MVVM.Services
 {
