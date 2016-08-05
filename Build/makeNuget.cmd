194246419c81478578d2c557354869ca2762925f
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-08-05 12:19:17 -0400

    Moved net461 to .csproj
    This means the other net461 applications can directly reference a
    project instead of using nuget packages for easier debugging and without
    the frustration of nuget package caching

--- Build/makeNuget.cmd
+++
@@ -1,7 +0,0 @@
-IF "%1" == "%2" (
-
-dotnet pack --no-build --configuration %1 -o ../%3
-
-xcopy %4 ".../%3/lib/%5/" /Y
-
-)
--- Contracts/Events/ItemSelectEvent.cs
+++ Contracts/Events/ItemSelectEvent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
-using Team1922.MVVM.Framework;
 
 namespace Team1922.MVVM.Contracts.Events
 {
