a32fec82d2c0f9a2a37e61ce98b32c83ab4f01ac
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-08-12 13:14:14 -0400

    Added Data Access Service
    This service is designed to hold the application data in a global
    setting.  Thead-safety must be considered eventually.  This is not yet
    configured with OzRobotBuilder.NET

---
+++ Contracts/IDataAccessService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team1922.MVVM.Contracts
+{
+    /// <summary>
+    /// This is a holding class for a data-access class
+    /// </summary>
+    public interface IDataAccessService
+    {
+        /// <summary>
+        /// The instace of the data to access
+        /// </summary>
+        IHierarchialAccess DataInstance { get; set; }
+    }
+}
---
+++ Services/DataAccessService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Team1922.MVVM.Contracts;
+
+namespace Team1922.MVVM.Services
+{
+    internal class DataAccessService : IDataAccessService
+    {
+        public IHierarchialAccess DataInstance { get; set; }
+    }
+}
