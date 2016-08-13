e2bebba8b5c20105fb010f629311e6197da39e4e
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-08-12 22:40:23 -0400

    Added Path Assertion to Data Access Service

--- Contracts/IDataAccessService.cs
+++ Contracts/IDataAccessService.cs
@@ -15,5 +15,10 @@ namespace Team1922.MVVM.Contracts
         /// The instace of the data to access
         /// </summary>
         IHierarchialAccess DataInstance { get; set; }
+        /// <summary>
+        /// This throws an exception if the given path is not valid
+        /// </summary>
+        /// <param name="path">the path to check</param>
+        void AssertPath(string path);
     }
 }
