48e5e0b442ea5f217c77f4fc1964468423cdbb58
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-08-24 19:25:38 -0400

    Moved IHierarchialAccessRoot Instance to RequestDelegator
    This was changed to make this as modular as possible.

--- WebHostFramework/RequestDelegator.cs
+++ WebHostFramework/RequestDelegator.cs
@@ -5,30 +5,66 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Team1922.MVVM.Contracts;
 using Team1922.MVVM.Models.XML;
 
 namespace Team1922.WebFramework
 {
-    public class RequestDelegator
+    public class RequestDelegator : IDisposable
     {
-        public static string ConvertPath(string key)
+        public RequestDelegator(IHierarchialAccessRoot data)
         {
-            return key.Replace('/', '.');
+            Data = data;
         }
 
-        public struct BasicHttpResponse
+        public IHierarchialAccessRoot Data
         {
-            public string Body { get; set; }
-            public int StatusCode { get; set; }
+            get
+            {
+                if (null == _data)
+                    throw new NullReferenceException("RequestDelegator.Data Must Be Initialized Before Using!");
+                return _data;
+            }
+
+            set
+            {
+                _data = value;
+            }
+        }
+        public async Task ProcessRequestAsync(HttpContext context)
+        {
+            //split the path after "api/Robot"
+            string fullRequestPath = context.Request.Path.Value;
+            string requestPath = GetPath(fullRequestPath);
+            if(null == requestPath)
+            {
+                //this means the path was invalid
+                context.Response.StatusCode = 404;
+                return;
+            }
+            requestPath = ConvertPath(requestPath);
+
+            //get the request body
+            var bufferReader = new StreamReader(context.Request.Body);            
+            string requestBody = await bufferReader.ReadToEndAsync();
+
+            //call the correct method for this request
+            BasicHttpResponse response = await AggregateMethodAsync(context.Request.Method, requestPath, requestBody);
+
+            //set the status code of the response
+            context.Response.StatusCode = response.StatusCode;   
+
+            //this needs to go last!
+            await context.Response.WriteAsync(response.Body ?? "");
         }
 
         #region CRUD Access Methods
-        protected async Task<BasicHttpResponse> GetAsync(string path)
+        private async Task<BasicHttpResponse> GetAsync(string path)
         {
             BasicHttpResponse response = new BasicHttpResponse();
             try
             {
-                response.Body = await RobotRepository.Instance.GetAsync(path);
+                response.Body = await Data.GetAsync(path);
 
                 //OK
                 response.StatusCode = 200;
@@ -45,17 +81,17 @@ namespace Team1922.WebFramework
             }
             return response;
         }
-        protected async Task<BasicHttpResponse> PostAsync(string path, string body)
+        private async Task<BasicHttpResponse> PostAsync(string path, string body)
         {
             BasicHttpResponse response = new BasicHttpResponse();
             try
             {
                 //only do this operation if it does not exists already
-                if (RobotRepository.Instance.KeyExists(path))
+                if (Data.KeyExists(path))
                     response.StatusCode = 409;
                 else
                 {
-                    await RobotRepository.Instance.SetAsync(path, body);
+                    await Data.SetAsync(path, body);
 
                     //created
                     response.StatusCode = 201;
@@ -79,13 +115,13 @@ namespace Team1922.WebFramework
             }
             return response;
         }
-        protected async Task<BasicHttpResponse> PutAsync(string path, string body)
+        private async Task<BasicHttpResponse> PutAsync(string path, string body)
         {
             BasicHttpResponse response = new BasicHttpResponse();
             try
             {
                 //set whether it exists or not
-                await RobotRepository.Instance.SetAsync(path, body);
+                await Data.SetAsync(path, body);
 
                 //created
                 response.StatusCode = 201;
@@ -108,16 +144,16 @@ namespace Team1922.WebFramework
             }
             return response;
         }
-        protected async Task<BasicHttpResponse> PatchAsync(string path, string body)
+        private async Task<BasicHttpResponse> PatchAsync(string path, string body)
         {
             BasicHttpResponse response = new BasicHttpResponse();
             try
             {
-                if (!RobotRepository.Instance.KeyExists(path))
+                if (!Data.KeyExists(path))
                     throw new ArgumentException();
 
                 //only do this operation if it exists already
-                await RobotRepository.Instance.SetAsync(path, body);
+                await Data.SetAsync(path, body);
 
                 //OK
                 response.StatusCode = 200;
@@ -140,13 +176,13 @@ namespace Team1922.WebFramework
             }
             return response;
         }
-        protected async Task<BasicHttpResponse> DeleteAsync(string path)
+        private async Task<BasicHttpResponse> DeleteAsync(string path)
         {
             BasicHttpResponse response = new BasicHttpResponse();
             try
             {
                 //null indicates we want this object to be deleted
-                await RobotRepository.Instance.SetAsync(path, null);
+                await Data.SetAsync(path, null);
 
                 //OK
                 response.StatusCode = 200;
@@ -176,40 +212,15 @@ namespace Team1922.WebFramework
         }
         #endregion
 
-        private string GetPath(string fullPath)
+        #region Private Helper Methods
+        private static string GetPath(string fullPath)
         {
             var split = fullPath.Split(new string[] { "api/Robot/" }, StringSplitOptions.None);
             if (split.Length != 2)
                 return null;
             return split[1];
         }
-        public async Task ProcessRequestAsync(HttpContext context)
-        {
-            //split the path after "api/Robot"
-            string fullRequestPath = context.Request.Path.Value;
-            string requestPath = GetPath(fullRequestPath);
-            if(null == requestPath)
-            {
-                //this means the path was invalid
-                context.Response.StatusCode = 404;
-                return;
-            }
-            requestPath = ConvertPath(requestPath);
-
-            //get the request body
-            var bufferReader = new StreamReader(context.Request.Body);            
-            string requestBody = await bufferReader.ReadToEndAsync();
-
-            //call the correct method for this request
-            BasicHttpResponse response = await AggregateMethodAsync(context.Request.Method, requestPath, requestBody);
-
-            //set the status code of the response
-            context.Response.StatusCode = response.StatusCode;   
-
-            //this needs to go last!
-            await context.Response.WriteAsync(response.Body ?? "");
-        }
-        public async Task<BasicHttpResponse> AggregateMethodAsync(string method, string requestPath, string requestBody)
+        private async Task<BasicHttpResponse> AggregateMethodAsync(string method, string requestPath, string requestBody)
         {
             switch (method)
             {
@@ -227,5 +238,58 @@ namespace Team1922.WebFramework
                     return new BasicHttpResponse() { StatusCode = 503 };//Is this the right status code?
             }
         }
+        private static string ConvertPath(string key)
+        {
+            return key.Replace('/', '.');
+        }
+        #endregion
+
+        #region Private Data Types
+        private struct BasicHttpResponse
+        {
+            public string Body { get; set; }
+            public int StatusCode { get; set; }
+        }
+        #endregion
+
+        #region Private Fields
+        private IHierarchialAccessRoot _data;
+        #endregion
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    Data.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~ViewModelBase() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
