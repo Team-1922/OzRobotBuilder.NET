823c590d9c9a200253564f5aefff4df9b1de394b
Kevin Mackenzie <dev.kjmack305@gmail.com>
2016-09-13 22:15:32 -0400

    Moved Protocall and Modifed Propagation Event Args
    In order to align with the new socket communications, the Propagation
    Event Args was modified to use the Protocall.Method enumeration instead
    of a string representation of HTTP verbs

--- Contracts/Protocall.cs
+++ Contracts/Protocall.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Team1922.WebFramework.Sockets
+namespace Team1922.MVVM.Contracts
 {
     public static class Protocall
     {
--- WebFramework/Sockets/Request.cs
+++ WebFramework/Sockets/Request.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Team1922.MVVM.Contracts;
 
 namespace Team1922.WebFramework.Sockets
 {
